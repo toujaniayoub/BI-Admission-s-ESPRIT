b7432befad352f71974c6b43f07a6d6a89ccaefe
NesrineHajri <nessrine.elhajri@gmail.com>
2025-05-10 15:18:43 +0200

    Improve template design and layout

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'; 
 
 @Component({
   selector: 'app-dashboard', 
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'] 
+  styleUrls: ['./dashboard.component.css'] ,
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit { 
 
@@ -13,16 +14,19 @@ export class DashboardComponent implements OnInit {
   constructor(private sanitizer: DomSanitizer) {} 
 
   ngOnInit(): void {
-    const unsafeUrl = 'https://app.powerbi.com/reportEmbed?reportId=fe6218bd-8305-448b-b6d0-21d2deb3512a&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730';
-
+    const unsafeUrl = 'https://app.powerbi.com/reportEmbed?reportId=fd9243e6-3c23-40e3-b52f-f659de6f22ee&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730&filterPaneEnabled=false&navContentPaneEnabled=false';
+  
     this.powerBiUrl = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl);
   }
+  
 
   selectedMenu: string = 'dashboard'; 
 
 showRecommendation() {
   this.selectedMenu = 'recommendation';
 }
+
+
 
 
 }
